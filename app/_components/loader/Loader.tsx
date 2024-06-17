ebf1a54a2d451c4f9eb78d668f53067603e5f604
Flovet-stack <lovetofloveto3@gnail.com>
2024-06-17 15:22:06 +0100

    app can now build

--- app/_components/loader/Loader.tsx
+++ app/_components/loader/Loader.tsx
@@ -1,46 +1,70 @@
 "use client";
 
 import { Variants, motion } from "framer-motion";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export const Loader = () => {
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
-  const width = window.innerWidth;
-  const height = window.innerHeight;
+  const [dimensions, setDimensions] = useState<{
+    width: number;
+    height: number;
+  }>({ width: 0, height: 0 });
 
-  function onSquareLoaded(callback: (isLoaded: boolean) => void) {
-    // Create a MutationObserver to watch for changes in the DOM
-    const observer = new MutationObserver((mutationsList) => {
-      for (let mutation of mutationsList) {
-        if (mutation.type === "childList") {
-          // Convert NodeList to Array before iterating
-          const addedNodes = Array.from(mutation.addedNodes);
-          for (let node of addedNodes) {
-            // Ensure the node is an instance of Element before accessing classList
-            if (node instanceof Element && node.classList.contains("square")) {
-              // Disconnect the observer since we only need to detect the first instance
-              observer.disconnect();
-              // Call the callback function
-              callback(true);
-              return;
+  useEffect(() => {
+    const handleResize = () => {
+      setDimensions({ width: window.innerWidth, height: window.innerHeight });
+    };
+
+    // Set initial dimensions
+    handleResize();
+
+    // Add event listener to update dimensions on window resize
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      // Clean up event listener on component unmount
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    function onSquareLoaded(callback: (isLoaded: boolean) => void) {
+      // Create a MutationObserver to watch for changes in the DOM
+      const observer = new MutationObserver((mutationsList) => {
+        for (let mutation of mutationsList) {
+          if (mutation.type === "childList") {
+            // Convert NodeList to Array before iterating
+            const addedNodes = Array.from(mutation.addedNodes);
+            for (let node of addedNodes) {
+              // Ensure the node is an instance of Element before accessing classList
+              if (
+                node instanceof Element &&
+                node.classList.contains("square")
+              ) {
+                // Disconnect the observer since we only need to detect the first instance
+                observer.disconnect();
+                // Call the callback function
+                callback(true);
+                return;
+              }
             }
           }
         }
+      });
+
+      // Start observing the document body for changes in child elements
+      observer.observe(document.body, { childList: true, subtree: true });
+    }
+
+    onSquareLoaded((isLoaded) => {
+      if (isLoaded) {
+        setIsLoaded(true);
       }
     });
-
-    // Start observing the document body for changes in child elements
-    observer.observe(document.body, { childList: true, subtree: true });
-  }
-
-  onSquareLoaded((isLoaded) => {
-    if (isLoaded) {
-      setIsLoaded(true);
-    }
-  });
+  }, []);
 
   const loaderVariants: Variants = {
-    loading: () => ({
+    loading: {
       width: 50,
       height: 50,
       borderWidth: "1000px",
@@ -51,10 +75,10 @@ export const Loader = () => {
       transition: {
         duration: 0,
       },
-    }),
-    animate: () => ({
-      width,
-      height,
+    },
+    animate: {
+      width: dimensions.width,
+      height: dimensions.height,
       borderWidth: ["1000px", "0px"],
       borderStyle: "solid",
       borderColor: "#b8ddef",
@@ -65,14 +89,13 @@ export const Loader = () => {
         ease: "easeInOut",
         times: [0, 1],
       },
-    }),
+    },
   };
 
   return (
     <motion.div
       className="loader-box"
       animate={isLoaded ? "animate" : "loading"}
-      //   animate={"loading"}
       variants={loaderVariants}
     />
   );
